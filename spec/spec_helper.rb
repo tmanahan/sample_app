4b3e48448774cd30ba380e06e5193d16a4ae2b35
Ted Manahan <ted.manahan@ars.usda.gov>
2011-12-01 11:25:28 -0700

    Exercises for Chapter 5

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,12 @@ require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environ
 require 'spec/autorun'
 require 'spec/rails'
 
+# webrat - will this work?
+require "webrat"
+
+Webrat.configure do |config|
+  config.mode = :rails
+end
 # Uncomment the next line to use webrat's matchers
 #require 'webrat/integrations/rspec-rails'
 
