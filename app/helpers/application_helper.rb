c91af1eec5a422faf455a7cca1eee07403f4ac1a
Ted Manahan <ted.manahan@ars.usda.gov>
2011-12-01 11:09:46 -0700

    Exercises for chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
