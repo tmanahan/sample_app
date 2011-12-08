5b9357a2af040ac9f2853706c3b94e97204d6c0e
Ted Manahan <ted.manahan@ars.usda.gov>
2011-12-08 14:25:38 -0700

    Done with user edit/update, index, and destroy actions

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@ RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+
+
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
@@ -15,6 +17,7 @@ Rails::Initializer.run do |config|
   # config.autoload_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
+  config.gem 'will_paginate', :version => '2.3.12'
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
